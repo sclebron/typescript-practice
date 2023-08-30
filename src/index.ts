3fc40fafee0181bd592faaa064a23eead2172c6d
SLeBron <sophieclebron@gmail.com>
2023-08-30 14:27:49 -0600

    practiced type aliases

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+}
 
-
+let employee: Employee = {
+    id: 1,
+    name: 'Sophie',
+    retire: (date: Date) => {
+        console.log(date);
+    }
+}
 
