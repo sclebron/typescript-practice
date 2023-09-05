fec9ca11ad30b8efc4062332c26dfadd8e392c23
SLeBron <sophieclebron@gmail.com>
2023-09-05 15:02:20 -0600

    practice null type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-type Quantity = 50 | 100;
-let quantity: Quantity = 50;
+function greet(name: string | null | undefined) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola!');
+}
 
-type Metric = 'cm' | 'inch';
+greet(null)
